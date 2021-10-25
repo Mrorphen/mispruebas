6c6bc509a0e9f9faed2c67121350e71ae02c227f
Mrorphen <44123433+Mrorphen@users.noreply.github.com>
2021-10-25 22:03:41 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 var caja = document.getElementById("goTop");
+var logo = document.getElementById("logo");
 
 caja.addEventListener("click", function(){
 	document.documentElement.scrollTop=0;
 });
 
 window.addEventListener("scroll", function(){
-	if(document.documentElement.scrollTop > 100){
+	if(document.documentElement.scrollTop > 50){
 		caja.style.display="flex";
+
 	}else{
 		caja.style.display="none";
 	}
